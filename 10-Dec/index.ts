46d472463023d4fd73eb5c55e91e46e6d5390674
Akbar Ali <m.akberali507@gmail.com>
2023-12-10 12:20:59 +0500

    Quatar 1

---
+++ 10-Dec/index.ts
@@ -0,0 +1,12 @@
+let names:string[] = ["Akbar Ali", "Kamran Hanif", "Mani", "Bilal"]
+for(let i =0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+// Splices Method in Array
+names.splice(2, 0, "Faizan", "Saad");
+console.log(names)
+
+// Slice
+let slnames = names.slice(1);
+console.log(slnames);
---
+++ 3-Dec/index.js
@@ -0,0 +1,23 @@
+"use strict";
+// Functions and function parameters
+function sum(n1, n2) {
+    let sum = n1 + n2;
+    return sum;
+}
+console.log(sum(2, 3));
+// Arrays
+let names = ["Akbar Ali", "Kamran Hanif", "Mani", "Bilal"];
+console.log(names);
+console.log(names[0]);
+// Push Method in Array
+names.push("Faizan");
+console.log(names);
+// Pop Method in Array
+names.pop();
+console.log(names);
+// Shift Method in Array
+names.shift();
+console.log(names);
+// UnShift Method in Array
+names.unshift("Akbar Ali");
+console.log(names);
